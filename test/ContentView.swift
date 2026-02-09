c6e2628c2e4062adb52c9d37ac25f290cbbed017
Mahir <srmahir98@gmail.com>
2026-02-09 17:24:05 +0600

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Mahir Shahriar on 9/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Mahir Shahriar on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
