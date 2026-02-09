66d9b264714df5e7d7a8205281ff4a4a7628bd86
Mahir <srmahir98@gmail.com>
2026-02-09 19:40:15 +0600

    Followed the third tutorial of CodeWithChris

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.mint)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20) {
+                Image("niagarafalls")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 16,style:.continuous))
+                HStack {
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .bold()
+                    Spacer()
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        Text("Review 361")
+                    }
+                    .foregroundStyle(.orange)
+                    .font(.caption)
+                }
+                Text("Come visit for an experience of a lifetime.")
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundStyle(.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(){
+                Rectangle()
+                    .foregroundStyle(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16,style:.continuous))
+                    .shadow(radius: 15)
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
